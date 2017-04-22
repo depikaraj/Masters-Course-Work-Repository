3f79ef3f698a13b6f57da0084b3b54848c977814
Deepika Raj <deepika.raj.93@gmail.com>
2017-04-22 10:28:57 +0530

    Log files attached

---
+++ Modern Operating Systems/Assignment 3/Prep/GDB/debug.c
@@ -0,0 +1,13 @@
+/*Sample program for understanding how GDB works */
+#include<stdio.h>
+
+int main()
+{
+	int a=0;
+	int b=1;
+	int c=2;
+	int d=3;
+	printf("Value of a is %d \n", a);
+	return 0;
+}
+	
